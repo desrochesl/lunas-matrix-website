9ec70911ec15d92bc1e04ba0baf2e5447b3e28d9
desrocheslatwit <luna@desroches.org>
2025-10-12 15:27:41 -0400

    ✨ Introduced: Add, Constant Multiply, and Multiply Matrices

--- src/matrix.js
+++
@@ -1,2 +0,0 @@
-document.getElementById('myH1').textContent = "Welcome to Luna DesRoches Matrices";
-document.getElementById('myP').textContent = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non alias in magni vero odit itaque! Officiis ab mollitia modi corporis distinctio adipisci magnam. Aut iste distinctio, delectus ut impedit repellendus.';
