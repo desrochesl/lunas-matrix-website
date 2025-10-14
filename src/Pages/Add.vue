0d4547486b947cf5e762aa849694b8f6021f71d3
desrocheslatwit <luna@desroches.org>
2025-10-14 03:24:17 -0400

    Reformatted

--- src/Pages/Add.vue
+++ src/Pages/Add.vue
@@ -17,20 +17,20 @@
     </label>
   </div>
   <div class="flex justify-center">
-    
-  <button class="btn m-4" onclick="my_modal_1.showModal()">Create Matrix</button>
+    <button class="btn m-4" onclick="my_modal_1.showModal()">
+      Create Matrix
+    </button>
   </div>
-
 
   <dialog id="my_modal_1" class="modal">
     <div class="modal-box">
-     <matrix-table :rows="rows" :cols="cols"></matrix-table>
-    <div class="modal-action">
-      <form method="dialog">
-        <!-- If there is a button in the form, it will close the modal -->
-        <button class="btn">Accept</button>
-      </form>
-    </div>
+      <matrix-table :rows="rows" :cols="cols"></matrix-table>
+      <div class="modal-action">
+        <form method="dialog">
+          <!-- If there is a button in the form, it will close the modal -->
+          <button class="btn">Accept</button>
+        </form>
+      </div>
     </div>
   </dialog>
 
@@ -42,12 +42,10 @@
 </template>
 
 <script>
-import Navbar from "../components/Navbar.vue";
-import Footer from "../components/Footer.vue";
 import MatrixTable from "../components/MatrixTable.vue";
 
 export default {
-  components: { Navbar, Footer, MatrixTable },
+  components: { MatrixTable },
   data() {
     return {
       rows: 3,
--- src/components/MatrixTable.vue
+++ src/components/MatrixTable.vue
@@ -1,13 +1,10 @@
 <template>
   <div class="p-0 mx-auto place-items-center">
+
     <div class="overflow-x-auto flex justify-center mt-8 mb-8">
       <table class="table border border-base-300 w-auto bg-base-100">
         <tbody>
-          <tr
-            class="w-20 h-20 text-center border border-base-300"
-            v-for="r in rows"
-            :key="r"
-          >
+          <tr class="w-20 h-20 text-center border border-base-300" v-for="r in rows" :key="r">
             <td v-for="c in cols" :key="c">
               <input
                 type="number"
@@ -23,7 +20,9 @@
 </template>
 
 <script>
+
 export default {
+
   props: {
     rows: {
       type: Number,
