1b2cce8dbbac8dabde1c9f15f2d17aac861ead9f
desrocheslatwit <luna@desroches.org>
2025-10-13 21:13:19 -0400

    🚧 Created Adding Function

---
+++ src/js/add.js
@@ -0,0 +1,4 @@
+import matrix from './matrixFunctions.js'
+
+console.log(matrix.addMatrix([[1,2,3]], [[1,2,3]]));
+
--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ createApp({
           title: "Add Matrices",
           description:
             "Combine matrices of equal dimensions by adding their elements.",
-          link: "/add",
+          link: "./matrixFunctions/add.html",
           buttonText: "Try Add",
         },
         {
