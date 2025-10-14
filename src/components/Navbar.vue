cdf7d8a713f701fe1e2d035df261cc358d2eb2d5
Luna <luna@desroches.org>
2025-10-14 18:47:40 -0400

    updated Navbar.vue
    fixed title button
    
    changed colors

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,8 +2,8 @@
   <div class="navbar bg-base-100 shadow-sm p-5">
     <div class="flex-1">
 
-      <a class="btn h-12 min-h-12 font-bold bg-base-100 btn-ghost text-3xl  hover:text-pink-700 rounded-md hover:bg-accent"
-         href="/">Luna's Matrix Calculator</a>
+      <a class="btn h-12 min-h-12 font-bold bg-base-100 btn-ghost text-3xl  hover: rounded-md hover:bg-primary"
+         href="/">Lunè's Matrix Calculator</a>
 
     </div>
     <div class="flex-none">
@@ -11,19 +11,19 @@
         <li>
           <details class="h-12 min-h-12 items-center ">
             <summary
-                class="h-12 min-h-12 px-4 flex font-bold rounded-md hover:bg-accent items-center justify-center hover:text-pink-700 "
+                class="h-12 min-h-12 px-4 flex font-bold rounded-md hover:bg-primary items-center justify-center hover: "
             >
               Functions
             </summary>
             <ul class="bg-base-100  p-2 absolute right-0 mt-2 shadow-lg z-50 ">
-              <li class="hover:text-pink-700 hover:bg-accent rounded-md"><a href="/add">Add</a></li>
+              <li class="hover: hover:bg-primary rounded-md"><a href="/add">Add</a></li>
               <!--              <li><a href="/multiply">Multiply</a></li>-->
               <!--              <li><a href="/rowreduce">Row Reduce</a></li>-->
               <!--              <li><a href="/determinant">Determinant</a></li>-->
             </ul>
           </details>
         </li>
-        <li class="hover:text-pink-800 hover:bg-accent rounded-md transition-colors">
+        <li class=" hover:bg-primary rounded-md transition-colors">
           <a
               class="flex items-center justify-center h-12 w-12"
               href="https://luna.desroches.org/github"
