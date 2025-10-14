932a315b1061795a1e9c78fb83ff10bc65fa99d8
desrocheslatwit <luna@desroches.org>
2025-10-14 18:44:57 -0400

    updated Navbar.vue
    fixed title button

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,26 +1,29 @@
 <template>
-  <div class="navbar bg-base-100 shadow-sm">
+  <div class="navbar bg-base-100 shadow-sm p-5">
     <div class="flex-1">
-      <a class="btn btn-ghost text-5xl" href="/">Matrices</a>
+
+      <a class="btn h-12 min-h-12 font-bold bg-base-100 btn-ghost text-3xl  hover:text-pink-700 rounded-md hover:bg-accent"
+         href="/">Luna's Matrix Calculator</a>
+
     </div>
     <div class="flex-none">
       <ul class="menu menu-horizontal px-1 items-center">
         <li>
-          <details class="h-12 min-h-12 items-center">
+          <details class="h-12 min-h-12 items-center ">
             <summary
-                class="h-12 min-h-12 px-4 flex font-bold items-center justify-center hover:text-pink-800"
+                class="h-12 min-h-12 px-4 flex font-bold rounded-md hover:bg-accent items-center justify-center hover:text-pink-700 "
             >
               Functions
             </summary>
-            <ul class="bg-base-100 rounded-t-none p-2 absolute right-0 mt-2 shadow-lg z-50">
-              <li><a href="/add">Add</a></li>
-<!--              <li><a href="/multiply">Multiply</a></li>-->
-<!--              <li><a href="/rowreduce">Row Reduce</a></li>-->
-<!--              <li><a href="/determinant">Determinant</a></li>-->
+            <ul class="bg-base-100  p-2 absolute right-0 mt-2 shadow-lg z-50 ">
+              <li class="hover:text-pink-700 hover:bg-accent rounded-md"><a href="/add">Add</a></li>
+              <!--              <li><a href="/multiply">Multiply</a></li>-->
+              <!--              <li><a href="/rowreduce">Row Reduce</a></li>-->
+              <!--              <li><a href="/determinant">Determinant</a></li>-->
             </ul>
           </details>
         </li>
-        <li class="hover:text-pink-800 transition-colors">
+        <li class="hover:text-pink-800 hover:bg-accent rounded-md transition-colors">
           <a
               class="flex items-center justify-center h-12 w-12"
               href="https://luna.desroches.org/github"
