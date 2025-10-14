4682f067893cc0ab839255eaf1ef10993043273f
desrocheslatwit <luna@desroches.org>
2025-10-14 02:47:43 -0400

    🎨 Restructured MatrixTable Template

--- src/Pages/Add.vue
+++ src/Pages/Add.vue
@@ -1,26 +1,44 @@
 <template>
   <div class="flex justify-center">
+    <label class="flex justify-center m-2">
+      <input
+        type="number"
+        v-model.number="rows"
+        min="1"
+        class="input w-10 text-center border border-base-300 m-2"
+      />
+      <p class="text mt-4">x</p>
+      <input
+        type="number"
+        v-model.number="cols"
+        min="1"
+        class="input w-10 text-center border border-base-300 m-2"
+      />
+    </label>
+  </div>
+  <div class="flex justify-center">
+    
   <button class="btn m-4" onclick="my_modal_1.showModal()">Create Matrix</button>
   </div>
 
 
   <dialog id="my_modal_1" class="modal">
     <div class="modal-box">
-     <matrix-table></matrix-table>
+     <matrix-table :rows="rows" :cols="cols"></matrix-table>
     <div class="modal-action">
       <form method="dialog">
         <!-- If there is a button in the form, it will close the modal -->
-        <button class="btn">Close</button>
+        <button class="btn">Accept</button>
       </form>
     </div>
     </div>
   </dialog>
 
-  <div class="flex flex-wrap">
+  <!-- <div class="flex flex-wrap">
     <matrix-table></matrix-table>
     <matrix-table></matrix-table>
     <matrix-table></matrix-table>
-  </div>
+  </div> -->
 </template>
 
 <script>
@@ -30,5 +48,11 @@ import MatrixTable from "../components/MatrixTable.vue";
 
 export default {
   components: { Navbar, Footer, MatrixTable },
+  data() {
+    return {
+      rows: 3,
+      cols: 3,
+    };
+  },
 };
 </script>
