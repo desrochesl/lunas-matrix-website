0f66e1d3cabae24ed9dc460ff29f2c62aac74531
desrocheslatwit <luna@desroches.org>
2025-10-14 20:10:50 -0400

    updated Add.vue
    Implemented scalable matrices

--- src/Pages/Add.vue
+++ src/Pages/Add.vue
@@ -1,17 +1,36 @@
 <template>
   <div class="flex justify-center items-center m-5">
+    <input
+        class="input validator input-xs m-2"
+        min="0"
+        type="number"
+        value="3"
+        v-model.number="rows"
+    />
+    <i class="fa-solid fa-xmark"></i>
+    <input
+        class="input validator input-xs m-2"
+        min="0"
+        type="number"
+        value="3"
+        v-model.number="cols"
+    />
+
+  </div>
+  <div class="flex justify-center items-center m-5">
+
     <button class="btn" @click="addMatrices">Add Matrices</button>
   </div>
   <div class="flex flex-wrap mt-0">
-    <matrix-table ref="matrix1" :cols="3" :rows="3"></matrix-table>
-    <matrix-table ref="matrix2" :cols="3" :rows="3"></matrix-table>
+    <matrix-table ref="matrix1" :cols="cols" :rows="rows"></matrix-table>
+    <matrix-table ref="matrix2" :cols="cols" :rows="rows"></matrix-table>
 
     <!-- Display result if it exists -->
     <matrix-table
         ref="resultTable"
-        :cols="3"
+        :cols="cols"
         :isReadOnly="true"
-        :rows="3">
+        :rows="rows">
     </matrix-table>
   </div>
 </template>
@@ -24,6 +43,8 @@ export default {
   components: {MatrixTable},
   data() {
     return {
+      rows: 3,
+      cols: 3,
       resultMatrix: [],
     };
   },
